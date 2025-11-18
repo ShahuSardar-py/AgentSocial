4988afdb60ff777d2ef25a3e05428a8f08ea2a53
ShahuSardar-py <shahu.sardar4@gmail.com>
2025-11-18 15:34:25 +0530

    fixed UI for script generation form

--- pages/2_▶_CreativeAgent.py
+++ pages/2_▶_CreativeAgent.py
@@ -1,14 +1,9 @@
 import streamlit as st
-from pytrends.request import TrendReq
-import pandas as pd
-import altair as alt
-import time
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 import os
-import requests
-from bs4 import BeautifulSoup
+
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GAPI"))
@@ -28,8 +23,15 @@ with st.form("script_gen", clear_on_submit=False):
                            ["Instagram reel", 
                             "Youtube Short", 
                             "Introductory pitch"])
-    
-    topic= st.text_input("Describe product/topic.")
+    col1,col2= st.columns(2)
+    with col1:
+         textt= st.text_area("enter")
+         topic= textt
+    with col2:
+         brochure= st.file_uploader(
+              "Uplaod Brochure",
+              type=["pdf", "png", "jpg", "jpeg"]
+         )
     tone= st.pills("Tone",["Casual",
                            "Energetic", 
                            "Emotional", 
@@ -46,7 +48,7 @@ with st.form("script_gen", clear_on_submit=False):
      "Relatable problem",
      "Bold claim"])
     
-
+  
     
     submitted= st.form_submit_button("Submit")
 
@@ -54,9 +56,11 @@ if submitted:
         prompt = f"""
             You are an expert script writer from a premium media house. You have been hired to write scripts for the users media production
             Create a strong script for {platform} video while keeping in mind the follwing personalizations user has decided upon:
-            product details/topic: {topic}
+            product details/topic: {topic or 'None'}
             tone: {tone or 'None'}
             hook = {hook_type}
+            brochure= {brochure}
+            If brochure content is provided, ignore the {topic} input entirely and treat the extracted brochure text as the only source of truth. Only create the script from the brochure content.
 
             Requirements:
             - Human-like, engaging script
@@ -64,7 +68,7 @@ if submitted:
             - aviod too much cliches 
             - avoid heavy wording unless necessary
             - Do NOT add any introductions, explanations, or phrases like:
-                "Sure, here’s your script", "Absolutely", "Here you go", 
+                "Sure, here's your script", "Absolutely", "Here you go", 
                 "This is a punchy script", or any conversational filler.
 
             - Start the script DIRECTLY with the first line of the actual script.
